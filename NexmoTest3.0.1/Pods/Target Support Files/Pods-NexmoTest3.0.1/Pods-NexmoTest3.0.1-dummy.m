4ed4ada21a1a054b23456ba61f764461ed88bafe
Mehboob Alam <mehboob.alam@vonage.com>
2023-02-14 18:40:23 +0100

    pods removed

--- NexmoTest3.0.1/Pods/Target Support Files/Pods-NexmoTest3.0.1/Pods-NexmoTest3.0.1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NexmoTest3_0_1 : NSObject
-@end
-@implementation PodsDummy_Pods_NexmoTest3_0_1
-@end
--- NexmoTest3.0.1/Pods/Target Support Files/Pods-NexmoTest3.0.1Tests/Pods-NexmoTest3.0.1Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NexmoTest3_0_1Tests : NSObject
-@end
-@implementation PodsDummy_Pods_NexmoTest3_0_1Tests
-@end
